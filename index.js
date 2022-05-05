a94aa0a02bd787ccabddcef5f58240886bafa3fd
toufiqul-hasan <toufiqul.shihab@gmail.com>
2022-05-05 09:48:06 +0600

    car collection added

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster.es8at.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("RoyalAuto").collection("car");
+
+    app.get("/car", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const car = await cursor.toArray();
+      res.send(car);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Royal Auto Server Running");
